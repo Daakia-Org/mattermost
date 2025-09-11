1b0f01157bb69acaf5d299b935994e6ab78cf4c7
Vashkarjya Das <vashkarjya.das@daakia.co.in>
2025-09-12 01:35:51 +0530

    Refactor ActionMenuClass to comment out marketplace functionality
    - Commented out the marketplace button and related system admin fallback in ActionMenuClass.
    - Updated logic to hide the actions menu if no plugin items are present, excluding the marketplace.
    - Cleaned up import statements for better clarity.

--- webapp/channels/src/components/actions_menu/actions_menu.tsx
+++ webapp/channels/src/components/actions_menu/actions_menu.tsx
@@ -25,8 +25,10 @@ import type {HandleBindingClick, OpenAppsModal, PostEphemeralCallResponseForPost
 import type {PostDropdownMenuAction, PostDropdownMenuItemComponent} from 'types/store/plugins';
 
 import ActionsMenuButton from './actions_menu_button';
-import ActionsMenuEmptyPopover from './actions_menu_empty_popover';
-import {ActionsMenuIcon} from './actions_menu_icon';
+
+// import ActionsMenuEmptyPopover from './actions_menu_empty_popover';
+
+// import {ActionsMenuIcon} from './actions_menu_icon';
 
 import './actions_menu.scss';
 
@@ -293,29 +295,33 @@ export class ActionMenuClass extends React.PureComponent<Props, State> {
 
         const {formatMessage} = this.props.intl;
 
-        let marketPlace = null;
-        if (this.props.canOpenMarketplace) {
-            marketPlace = (
-                <React.Fragment key={'marketplace'}>
-                    {this.renderDivider('marketplace')}
-                    <Menu.ItemAction
-                        id={`marketplace_icon_${this.props.post.id}`}
-                        key={`marketplace_${this.props.post.id}`}
-                        show={true}
-                        text={formatMessage({id: 'post_info.marketplace', defaultMessage: 'App Marketplace'})}
-                        icon={<ActionsMenuIcon name='icon-view-grid-plus-outline'/>}
-                        onClick={this.handleOpenMarketplace}
-                    />
-                </React.Fragment>
-            );
-        }
+        const marketPlace = null;
+
+        // Commented out marketplace button
+        // if (this.props.canOpenMarketplace) {
+        //     marketPlace = (
+        //         <React.Fragment key={'marketplace'}>
+        //             {this.renderDivider('marketplace')}
+        //             <Menu.ItemAction
+        //                 id={`marketplace_icon_${this.props.post.id}`}
+        //                 key={`marketplace_${this.props.post.id}`}
+        //                 show={true}
+        //                 text={formatMessage({id: 'post_info.marketplace', defaultMessage: 'App Marketplace'})}
+        //                 icon={<ActionsMenuIcon name='icon-view-grid-plus-outline'/>}
+        //                 onClick={this.handleOpenMarketplace}
+        //             />
+        //         </React.Fragment>
+        //     );
+        // }
 
         const hasApps = Boolean(appBindings.length);
         const hasPluggables = Boolean(this.props.pluginMenuItemComponents?.length);
         const hasPluginItems = Boolean(pluginItems?.length);
 
         const hasPluginMenuItems = hasPluginItems || hasApps || hasPluggables;
-        if (!this.props.canOpenMarketplace && !hasPluginMenuItems) {
+
+        // Hide actions menu entirely if no plugin items (ignoring marketplace)
+        if (!hasPluginMenuItems) {
             return null;
         }
 
@@ -359,26 +365,28 @@ export class ActionMenuClass extends React.PureComponent<Props, State> {
                     </Menu>
                 </MenuWrapper>
             );
-        } else if (this.props.isSysAdmin) {
-            return (
-                <>
-
-                    <ActionsMenuButton
-                        ref={this.buttonRef}
-                        buttonId={buttonId}
-                        onClick={this.openDropdown}
-                        popupId={popupId}
-                        isMenuOpen={this.props.isMenuOpen}
-                    />
-                    <ActionsMenuEmptyPopover
-                        anchorElement={this.buttonElement}
-                        onOpenMarketplace={this.handleOpenMarketplace}
-                        onToggle={this.props.handleDropdownOpened}
-                        isOpen={this.props.isMenuOpen}
-                    />
-                </>
-            );
         }
+
+        // Commented out system admin marketplace fallback
+        // else if (this.props.isSysAdmin) {
+        //     return (
+        //         <>
+        //             <ActionsMenuButton
+        //                 ref={this.buttonRef}
+        //                 buttonId={buttonId}
+        //                 onClick={this.openDropdown}
+        //                 popupId={popupId}
+        //                 isMenuOpen={this.props.isMenuOpen}
+        //             />
+        //             <ActionsMenuEmptyPopover
+        //                 anchorElement={this.buttonElement}
+        //                 onOpenMarketplace={this.handleOpenMarketplace}
+        //                 onToggle={this.props.handleDropdownOpened}
+        //                 isOpen={this.props.isMenuOpen}
+        //             />
+        //         </>
+        //     );
+        // }
 
         return null;
     }
