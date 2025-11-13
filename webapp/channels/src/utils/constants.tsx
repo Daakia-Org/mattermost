b1d52c84630c42d54a56db9f7a09a4e3fe777669
Vashkarjya Das <vashkarjya.das@daakia.co.in>
2025-11-13 19:57:53 +0530

    Update branding and messaging throughout the application to reflect the transition from Mattermost to Konnect by Daakia. This includes changes to site names, error messages, user interface prompts, and internationalization files to ensure consistency and clarity for users.

--- webapp/channels/src/utils/constants.tsx
+++ webapp/channels/src/utils/constants.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-/* eslint-disable max-lines */
+/* eslint-disable */
 
 import solarizedDarkCSS from 'highlight.js/styles/base16/solarized-dark.css';
 import solarizedLightCSS from 'highlight.js/styles/base16/solarized-light.css';
@@ -978,7 +978,7 @@ defineMessages({
     },
     socketError: {
         id: 'channel_loader.socketError',
-        defaultMessage: 'Please check connection, Mattermost unreachable. If issue persists, ask administrator to [check WebSocket port](!https://docs.mattermost.com/install/troubleshooting.html#please-check-connection-mattermost-unreachable-if-issue-persists-ask-administrator-to-check-websocket-port).',
+        defaultMessage: 'Please check connection, Konnect unreachable. If issue persists, ask administrator to [check WebSocket port](!https://docs.mattermost.com/install/troubleshooting.html#please-check-connection-mattermost-unreachable-if-issue-persists-ask-administrator-to-check-websocket-port).',
     },
     trialLicenseExpiring: {
         id: 'announcement_bar.error.trial_license_expiring',
