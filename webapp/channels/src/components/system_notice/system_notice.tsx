442f689b5c6064f033af03fb531667e0ccebee04
Vashkarjya Das <vashkarjya.das@daakia.co.in>
2025-11-14 00:18:48 +0530

    Comment out the SystemNotice component for Konnect by Daakia, disabling its functionality and returning null. This change streamlines the user experience by removing unnecessary notices.

--- webapp/channels/src/components/system_notice/system_notice.tsx
+++ webapp/channels/src/components/system_notice/system_notice.tsx
@@ -1,170 +1,186 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// System notice disabled for Konnect by Daakia - always return null
+// import React from 'react';
+// import {FormattedMessage, injectIntl, type WrappedComponentProps} from 'react-intl';
+// import type {AnalyticsState} from '@mattermost/types/admin';
+// import type {Channel} from '@mattermost/types/channels';
+// import type {ClientConfig, ClientLicense} from '@mattermost/types/config';
+// import type {PreferenceType} from '@mattermost/types/preferences';
+// import type {Notice} from 'components/system_notice/types';
+// import MattermostLogo from 'components/widgets/icons/mattermost_logo';
+// import {Preferences} from 'utils/constants';
+
 import React from 'react';
-import {FormattedMessage, injectIntl, type WrappedComponentProps} from 'react-intl';
+import {injectIntl} from 'react-intl';
+import type {WrappedComponentProps} from 'react-intl';
 
-import type {AnalyticsState} from '@mattermost/types/admin';
-import type {Channel} from '@mattermost/types/channels';
-import type {ClientConfig, ClientLicense} from '@mattermost/types/config';
-import type {PreferenceType} from '@mattermost/types/preferences';
-
-import type {Notice} from 'components/system_notice/types';
-import MattermostLogo from 'components/widgets/icons/mattermost_logo';
-
-import {Preferences} from 'utils/constants';
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export interface Props extends WrappedComponentProps {
     currentUserId: string;
-    notices: Notice[];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    notices: any[];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     preferences: {[key: string]: any};
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     dismissedNotices: any;
     isSystemAdmin?: boolean;
     serverVersion: string;
-    config: Partial<ClientConfig>;
-    license: ClientLicense;
-    analytics?: AnalyticsState;
-    currentChannel?: Channel;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    config: Partial<any>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    license: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    analytics?: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    currentChannel?: any;
     actions: {
-        savePreferences(userId: string, preferences: PreferenceType[]): void;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        savePreferences(userId: string, preferences: any[]): void;
         dismissNotice(type: string): void;
         getStandardAnalytics(teamId?: string): void;
     };
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export class SystemNotice extends React.PureComponent<Props> {
-    componentDidMount() {
-        if (this.props.isSystemAdmin) {
-            this.props.actions.getStandardAnalytics();
-        }
-    }
-
-    componentDidUpdate(prevProps: Props) {
-        if (prevProps.isSystemAdmin !== this.props.isSystemAdmin && this.props.isSystemAdmin) {
-            this.props.actions.getStandardAnalytics();
-        }
-    }
-
-    getCurrentNotice = () => {
-        for (const notice of this.props.notices) {
-            // Skip if dismissed previously this session
-            if (this.props.dismissedNotices[notice.name]) {
-                continue;
-            }
-
-            // Skip if dismissed forever
-            if (this.props.preferences[notice.name]) {
-                continue;
-            }
-
-            if (notice.adminOnly && !this.props.isSystemAdmin) {
-                continue;
-            }
-
-            if (!notice.show?.(
-                this.props.serverVersion,
-                this.props.config,
-                this.props.license,
-                this.props.analytics,
-                this.props.currentChannel,
-            )) {
-                continue;
-            }
-
-            return notice;
-        }
-        return null;
-    };
-
-    hide = (remind = false) => {
-        const notice = this.getCurrentNotice();
-        if (!notice) {
-            return;
-        }
-
-        if (!remind) {
-            this.props.actions.savePreferences(this.props.currentUserId, [{
-                user_id: this.props.currentUserId,
-                category: Preferences.CATEGORY_SYSTEM_NOTICE,
-                name: notice.name,
-                value: 'dismissed',
-            }]);
-        }
-
-        this.props.actions.dismissNotice(notice.name);
-    };
-
-    hideAndRemind = () => {
-        this.hide(true);
-    };
-
-    hideAndForget = () => {
-        this.hide(false);
-    };
-
     render() {
-        const notice = this.getCurrentNotice();
+        // System notice disabled for Konnect by Daakia - always return null
+        return null;
 
-        if (notice == null) {
-            return null;
-        }
+        // Original code commented out
+        // componentDidMount() {
+        //     if (this.props.isSystemAdmin) {
+        //         this.props.actions.getStandardAnalytics();
+        //     }
+        // }
 
-        let visibleMessage;
-        if (notice.adminOnly) {
-            visibleMessage = (
-                <div className='system-notice__info'>
-                    <i
-                        className='fa fa-eye'
-                        title={this.props.intl.formatMessage({id: 'system_notice.adminVisible.icon', defaultMessage: 'Only visible to System Admins Icon'})}
-                    />
-                    <FormattedMessage
-                        id='system_notice.adminVisible'
-                        defaultMessage='Only visible to System Admins'
-                    />
-                </div>
-            );
-        }
+        // componentDidUpdate(prevProps: Props) {
+        //     if (prevProps.isSystemAdmin !== this.props.isSystemAdmin && this.props.isSystemAdmin) {
+        //         this.props.actions.getStandardAnalytics();
+        //     }
+        // }
 
-        const icon = notice.icon || <MattermostLogo/>;
+        // getCurrentNotice = () => {
+        //     for (const notice of this.props.notices) {
+        //         // Skip if dismissed previously this session
+        //         if (this.props.dismissedNotices[notice.name]) {
+        //             continue;
+        //         }
 
-        return (
-            <div
-                className='system-notice bg--white'
-            >
-                <div className='system-notice__logo'>
-                    {icon}
-                </div>
-                <div className='system-notice__body'>
-                    <div className='system-notice__title'>
-                        {notice.title}
-                    </div>
-                    {notice.body}
-                    {visibleMessage}
-                    <div className='system-notice__footer'>
-                        <button
-                            id='systemnotice_remindme'
-                            className='btn btn-sm btn-primary'
-                            onClick={this.hideAndRemind}
-                        >
-                            <FormattedMessage
-                                id='system_notice.remind_me'
-                                defaultMessage='Remind Me Later'
-                            />
-                        </button>
-                        {notice.allowForget &&
-                            <button
-                                id='systemnotice_dontshow'
-                                className='btn btn-sm btn-tertiary'
-                                onClick={this.hideAndForget}
-                            >
-                                <FormattedMessage
-                                    id='system_notice.dont_show'
-                                    defaultMessage="Don't Show Again"
-                                />
-                            </button>}
-                    </div>
-                </div>
-            </div>
-        );
+        //         // Skip if dismissed forever
+        //         if (this.props.preferences[notice.name]) {
+        //             continue;
+        //         }
+
+        //         if (notice.adminOnly && !this.props.isSystemAdmin) {
+        //             continue;
+        //         }
+
+        //         if (!notice.show?.(
+        //             this.props.serverVersion,
+        //             this.props.config,
+        //             this.props.license,
+        //             this.props.analytics,
+        //             this.props.currentChannel,
+        //         )) {
+        //             continue;
+        //         }
+
+        //         return notice;
+        //     }
+        //     return null;
+        // };
+
+        // hide = (remind = false) => {
+        //     const notice = this.getCurrentNotice();
+        //     if (!notice) {
+        //         return;
+        //     }
+
+        //     if (!remind) {
+        //         this.props.actions.savePreferences(this.props.currentUserId, [{
+        //             user_id: this.props.currentUserId,
+        //             category: Preferences.CATEGORY_SYSTEM_NOTICE,
+        //             name: notice.name,
+        //             value: 'dismissed',
+        //         }]);
+        //     }
+
+        //     this.props.actions.dismissNotice(notice.name);
+        // };
+
+        // hideAndRemind = () => {
+        //     this.hide(true);
+        // };
+
+        // hideAndForget = () => {
+        //     this.hide(false);
+        // };
+
+        // const notice = this.getCurrentNotice();
+        // if (notice == null) {
+        //     return null;
+        // }
+
+        // let visibleMessage;
+        // if (notice.adminOnly) {
+        //     visibleMessage = (
+        //         <div className='system-notice__info'>
+        //             <i
+        //                 className='fa fa-eye'
+        //                 title={this.props.intl.formatMessage({id: 'system_notice.adminVisible.icon', defaultMessage: 'Only visible to System Admins Icon'})}
+        //             />
+        //             <FormattedMessage
+        //                 id='system_notice.adminVisible'
+        //                 defaultMessage='Only visible to System Admins'
+        //             />
+        //         </div>
+        //     );
+        // }
+
+        // const icon = notice.icon || <MattermostLogo/>;
+
+        // return (
+        //     <div
+        //         className='system-notice bg--white'
+        //     >
+        //         <div className='system-notice__logo'>
+        //             {icon}
+        //         </div>
+        //         <div className='system-notice__body'>
+        //             <div className='system-notice__title'>
+        //                 {notice.title}
+        //             </div>
+        //             {notice.body}
+        //             {visibleMessage}
+        //             <div className='system-notice__footer'>
+        //                 <button
+        //                     id='systemnotice_remindme'
+        //                     className='btn btn-sm btn-primary'
+        //                     onClick={this.hideAndRemind}
+        //                 >
+        //                     <FormattedMessage
+        //                         id='system_notice.remind_me'
+        //                         defaultMessage='Remind Me Later'
+        //                     />
+        //                 </button>
+        //                 {notice.allowForget &&
+        //                     <button
+        //                         id='systemnotice_dontshow'
+        //                         className='btn btn-sm btn-tertiary'
+        //                         onClick={this.hideAndForget}
+        //                     >
+        //                         <FormattedMessage
+        //                             id='system_notice.dont_show'
+        //                             defaultMessage="Don't Show Again"
+        //                         />
+        //                     </button>}
+        //             </div>
+        //         </div>
+        //     </div>
+        // );
     }
 }
 
